1ab40ed8ab27dcaf3d58225bc91ff44011df6e45
ron sutherland <ronald.sutherland@gmail.com>
2019-03-22 15:36:39 -0700

    libraries pin related need correct license

--- lib/pins_board.h
+++ lib/pins_board.h
@@ -1,20 +1,20 @@
 /*
  * Pin definitions for RPUno used with pin_num.h Digital IO library
- * Copyright (C) 2016 Ronald Sutherland
- *
- * This Library is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This Library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License 
- * along with the DigitalIO Library.  If not, see
- * <http://www.gnu.org/licenses/>.
+  Copyright (C) 2019 Ronald Sutherland
+ 
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
 #ifndef Pins_Board_h
 #define Pins_Board_h
